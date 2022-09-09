09b13b26a763cee1a51fd3cd399d0264d1091623
NokwethembaDlamini <mballyokhanyoe@gmail.com>
2022-09-08 21:13:23 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,19 +3,12 @@
 
 
 /**
+ *main - Print
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ *Return: Always 0 (Success)
+ */
 int main(void)
-
 {
             puts("\"Programming is like building a multilingual puzzle");
 	    return (0);
-
 }
