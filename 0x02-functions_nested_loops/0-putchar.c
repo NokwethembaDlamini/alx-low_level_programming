e0857d535cb29a84af783c12e1845a4bb857d1ca
NokwethembaDlamini <mballyokhanyoe@gmail.com>
2022-09-13 04:53:17 -0700

    Putchar 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,20 +2,21 @@
 
 /**
  *main - prints putchar
- *Return: Always 0
+ *
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
 	int _putchar(char c);
 
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
