cb212cd32b72276bc655d1ed474ca2c58b0cf133
NokwethembaDlamini <mballyokhanyoe@gmail.com>
2022-09-13 00:29:37 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+
+
+/**
+ *
+ *main - prints putchar
+ *Return: Always 0
+ */
+int main(void)
+{
+	int putchar(char c);
+
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	putchar("\n");
+	return (0);
+}
