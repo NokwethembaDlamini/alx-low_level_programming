8269167835bf983b329193ffca5435689d52afdb
NokwethembaDlamini <mballyokhanyoe@gmail.com>
2022-09-14 00:58:28 -0700

    Islower.C

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  *_islower - Entry point
- *Description: checks is a character is lowercase
+ *Description: checks if a character is lowercase
  *@c: the integer value it receives
  *Return: 1 if true. 0 if false.
  */
