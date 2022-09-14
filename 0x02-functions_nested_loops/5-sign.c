c935c10ba391566f55975ed527ece131c9ea3347
NokwethembaDlamini <mballyokhanyoe@gmail.com>
2022-09-14 01:53:46 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *print_sign - prints a sign based on a condition
+ *print_sign -> prints a sign based on a condition
  *@n: argument passed
- *Return: 1, 0, -1
+ *Return: 1, 0 -1
  */
 int print_sign(int n)
 {
