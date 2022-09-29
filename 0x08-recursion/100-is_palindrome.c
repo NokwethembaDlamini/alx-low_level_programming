53ea7c60dae68db547af81a7bc1752caceb5bff1
NokwethembaDlamini <mballyokhanyoe@gmail.com>
2022-09-29 07:10:26 -0700

    Advanced Task1

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,8 +13,8 @@ int is_palindrome(char *s)
 {
 	if (*s == 0)
 
-		return (1)
-			;return (check_pal(s, 0, _strlen_recursion(s)));
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
